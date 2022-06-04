cbeb86384d936cf854d33eabd8796090e59862e1
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-04 19:49:56 +0300

    Add Workday model

---
+++ Domain/Workday.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Workday
+    {
+        public Guid Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid EmployeeId { get; set; }
+        public int RoleID { get; set; }
+    }
+}
