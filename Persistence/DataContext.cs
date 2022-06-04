3d1071e1cb4b7b5fb4fe2ede9c831fcea09ea980
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-04 19:50:08 +0300

    Add DataContext

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Domain;
+
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> opt) : base(opt)
+        {
+        }
+
+        public DbSet<Workday> Workdays { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+
+    }
+}
