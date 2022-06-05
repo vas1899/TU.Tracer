57f79aa7e0561747e268a474aa270fc8061386ff
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-05 20:15:32 +0300

    Add Automapper

---
+++ Application/Core/AutomapperProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Domain;
+
+namespace Application.Core
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Workday, Workday>();
+        }
+    }
+}
