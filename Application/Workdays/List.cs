e4e95554a41d6183b9d6eacf26f7510eb7ceaf5b
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-05 20:15:00 +0300

    Add Workday list handler

---
+++ Application/Workdays/List.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Workdays
+{
+    public class List
+    {
+        public class Query : IRequest<List<Workday>> { }
+
+        public class Handler : IRequestHandler<Query, List<Workday>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<List<Workday>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var workdays = await _context.Workdays.ToListAsync(cancellationToken);
+                return workdays;
+            }
+
+        }
+    }
+}
