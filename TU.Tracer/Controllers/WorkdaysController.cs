62edf98a849c2dc669d5cd0714fa4df1d073afdc
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-05 21:24:26 +0300

    Add Workdays Controller

---
+++ TU.Tracer/Controllers/WorkdaysController.cs
@@ -0,0 +1,20 @@
+﻿using Application.Workdays;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TU.Tracer.Controllers
+{
+    public class WorkdaysController : BaseApiController
+    {
+
+        // GET: api/Workdays
+        [HttpGet]
+        public async Task<ActionResult<List<Workday>>> List(CancellationToken ct)
+        {
+            return await Mediator.Send(new List.Query(), ct);
+        }
+    }
+}
