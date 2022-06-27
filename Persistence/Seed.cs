6178856a133c3ef743b37f262957ee99125ad2a3
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-28 01:12:55 +0300

    Migrate Seed Users

---
+++ Persistence/Seed.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedDateAsync(DataContext datacontext, UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any()) {
+                var users = new List<AppUser> {
+                   new AppUser{DisplayName="Vasil", UserName="vasil", Email="vasil1212@gmail.com"},
+                   new AppUser{DisplayName="Vasil", UserName="vasil1", Email="vasil12321312@gmail.com"},
+                   new AppUser{DisplayName="Vasil", UserName="vasil2", Email="vasil1213213122@gmail.com"}
+                };
+                foreach (var user in users) {
+                    await userManager.CreateAsync(user, "123456");
+                }
+            }
+        }
+    }
+}
--- TU.Tracer/Program.cs
+++ TU.Tracer/Program.cs
@@ -1,25 +1,42 @@
+using Domain;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistence;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace TU.Tracer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Creates a database inside our project
+            using (var scope = host.Services.CreateScope()) {
+                var servises = scope.ServiceProvider;
+                try {
+                    var context = servises.GetRequiredService<DataContext>();
+                    var userManager = servises.GetRequiredService<UserManager<AppUser>>();
+                    await context.Database.MigrateAsync();
+                    await Seed.SeedDateAsync(context, userManager);
+                }
+                catch (Exception ex) {
+                    var logger = servises.GetRequiredService<Logger<Program>>();
+                    logger.LogError(ex, "Error during migration!!");
+                }
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
+                .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
     }
