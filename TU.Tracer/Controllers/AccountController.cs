ea9b38951af8d0d75766e6133c4f7a461403aab8
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-28 17:13:03 +0300

    Add POST login

---
+++ TU.Tracer/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TU.Tracer.DTOs;
+
+namespace TU.Tracer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        // POST: api/Accounts
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null) {
+                return Unauthorized();
+            }
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (result.Succeeded) {
+                return new UserDto {
+                    DisplayName = user.DisplayName,
+                    UserName = user.UserName,
+                    Token = "token",
+                    Image = null
+                };
+            }
+            return Unauthorized();
+
+        }
+
+    }
+}
