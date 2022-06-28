7bc276915d1c18d5213e580cac36b40ff62ed644
Vasil Kolev <vasil_kolev99@abv.bg>
2022-06-28 17:12:49 +0300

    Add user login dtos

---
+++ TU.Tracer/DTOs/RegisterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TU.Tracer.DTOs
+{
+    public class RegisterDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
