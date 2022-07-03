5ecb393c9d4924f63e1458a06dc05861023a23c8
Vasil Kolev <vasil_kolev99@abv.bg>
2022-07-04 01:17:18 +0300

    Seed packet data

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -10,7 +10,7 @@ namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedDateAsync(DataContext datacontext, UserManager<AppUser> userManager)
+        public static async Task SeedDateAsync(DataContext dataContext, UserManager<AppUser> userManager)
         {
             if (!userManager.Users.Any()) {
                 var users = new List<AppUser> {
@@ -22,6 +22,119 @@ namespace Persistence
                     await userManager.CreateAsync(user, "123456");
                 }
             }
+
+            if (!dataContext.Packets.Any()) {
+                var Packets = new List<Packet>
+                {
+                    new Packet
+                    {
+                        Title = "Past Packet 1",
+                        Date = DateTime.Now.AddMonths(-2),
+                        Description = "Packet 2 months ago",
+                        Category = "drinks",
+
+                        IsDelivered = false,
+                    },
+                    new Packet
+                    {
+                        Title = "Past Packet 2",
+                        Date = DateTime.Now.AddMonths(-1),
+                        Description = "Packet 1 month ago",
+                        Category = "culture",
+                        IsDelivered = false,
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 1",
+                        Date = DateTime.Now.AddMonths(1),
+                        Description = "Packet 1 month in future",
+                        Category = "culture",
+
+                        IsDelivered = false,
+
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 2",
+                        Date = DateTime.Now.AddMonths(2),
+                        Description = "Packet 2 months in future",
+                        Category = "music",
+
+                        IsDelivered = false,
+
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 3",
+                        Date = DateTime.Now.AddMonths(3),
+                        Description = "Packet 3 months in future",
+                        Category = "drinks",
+
+                        IsDelivered = false,
+
+
+                    },
+                    new    Packet
+                    {
+                        Title = "Future Packet 4",
+                        Date = DateTime.Now.AddMonths(4),
+                        Description = "Packet 4 months in future",
+                        Category = "drinks",
+
+                        IsDelivered = false,
+
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 5",
+                        Date = DateTime.Now.AddMonths(5),
+                        Description = "Packet 5 months in future",
+                        Category = "drinks",
+
+                        IsDelivered = false,
+
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 6",
+                        Date = DateTime.Now.AddMonths(6),
+                        Description = "Packet 6 months in future",
+                        Category = "music",
+
+                        IsDelivered = false,
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 7",
+                        Date = DateTime.Now.AddMonths(7),
+                        Description = "Packet 2 months ago",
+                        Category = "travel",
+
+                        IsDelivered = false,
+
+                    },
+                    new Packet
+                    {
+                        Title = "Future Packet 8",
+                        Date = DateTime.Now.AddMonths(8),
+                        Description = "Packet 8 months in future",
+                        Category = "film",
+
+                        IsDelivered = false,
+
+                    }
+                };
+
+                dataContext.Packets.AddRange(Packets);
+                dataContext.SaveChanges();
+            }
+
         }
     }
 }
