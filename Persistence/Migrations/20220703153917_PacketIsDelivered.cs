7c2369ac4145d3d7b616ec334b143483906f6309
Vasil Kolev <vasil_kolev99@abv.bg>
2022-07-04 01:17:44 +0300

    Migration add Packet IsDelivered

---
+++ Persistence/Migrations/20220703153917_PacketIsDelivered.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class PacketIsDelivered : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelivered",
+                table: "Packets",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelivered",
+                table: "Packets");
+        }
+    }
+}
