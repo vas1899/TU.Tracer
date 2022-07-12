ff70aa32674751d7ef49285d03ae2349bbdddfdb
Vasil Kolev <vasil_kolev99@abv.bg>
2022-07-13 00:12:59 +0300

    Add pages list class

---
+++ Application/Core/PagedList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Core
+{
+    public class PagedList<T> : List<T>
+    {
+        public PagedList(IEnumerable<T> items, int currentPage, int pageSize, int itemsCount)
+        {
+            CurrentPage = currentPage;
+            TotalPages = (int)Math.Ceiling(itemsCount / (double)pageSize);
+            PageSize = pageSize;
+            ItemsCount = itemsCount;
+            AddRange(items);
+        }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; }
+        public int ItemsCount { get; set; }
+
+        public static async Task<PagedList<T>> CreateAsync(IQueryable<T> source, int currentPage, int pageSize)
+        {
+            int itemsCount = await source.CountAsync();
+            var items = await source.Skip(pageSize * (currentPage-1)).Take(pageSize).ToListAsync();
+
+            return new PagedList<T>(items, currentPage, pageSize, itemsCount);
+        }
+
+    }
+}
